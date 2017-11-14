fbfe2d2257fd98973fd106aefa2ad7795c2fbbeb
Wu-Tang-Name-Here <einarsevilla@gmail.com>
2017-11-13 19:02:55 -0800

    Add files via upload

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -3,34 +3,34 @@ var initialLocations = [
 	{
 			name: 'La Taqueria', 
 			address: '2889 Mission St, San Francisco, CA', 
-			latLng: {lat: 37.751087, lng: -122.418092}
+			coordinates: {lat: 37.751087, lng: -122.418092}
 		},
 		{
 			name: 'Taqueria El Farolito', 
 			address: '2779 Mission St, San Francisco, CA', 
-			latLng: {lat: 37.752938, lng:  -122.418218}
+			coordinates: {lat: 37.752938, lng:  -122.418218}
 		},
 		{
 			name: 'Taqueria La Cumbre', 
 			address: '515 Valencia St, San Francisco, CA', 
-			latLng: {lat: 37.764852, lng: -122.421671}
+			coordinates: {lat: 37.764852, lng: -122.421671}
 		},
 		{
 			name: 'El Faro', 
 			address: '2399 Folsom St, San Francisco, CA', 
-			latLng: {lat: 37.759231, lng: -122.414514}
+			coordinates: {lat: 37.759231, lng: -122.414514}
 		},
 		{
 			name: 'Taqueria Vallarta', 
 			address: '3033 24th St, San Francisco, CA', 
-			latLng: {lat: 37.752637, lng: -122.412566}
+			coordinates: {lat: 37.752637, lng: -122.412566}
 		}
 ]
 
 var Location = function(data) {
 	this.name = ko.observable(data.name);
 	this.address = ko.observable(data.address);
-	this.latLng = ko.observable(data.latLng);
+	this.coordinates = ko.observable(data.coordinates);
 };
 
 var ViewModel = function(){
@@ -39,7 +39,7 @@ var ViewModel = function(){
 	this.locationList = ko.observable([]);
 
 	initialLocations.forEach(function(locationItem){
-		self.locationList.push( new Location(locationItem) );
+		this.locationList = ko.observableArray([]);
 	});
 
 	this.currentLocation = ko.observable( this.locationList()[0] );
@@ -47,33 +47,54 @@ var ViewModel = function(){
 	this.setLocation = function(clickedLoc) {
 		self.currentLocation(clickedLoc);
 	};
+
+	/////MAP//////
+	/*self.myMap = ko.observable({
+		lat: ko.observable(37.759951),
+		lng: ko.observable(-122.415139)
+	})*/
 };
 
 ///////////View/////////////////////////
+/*ko.bindingHandlers.map = {
+
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var mapObj = ko.utils.unwrapObservable(valueAccessor());
+        var latLng = new google.maps.LatLng(
+            ko.utils.unwrapObservable(mapObj.lat),
+            ko.utils.unwrapObservable(mapObj.lng));
+        var mapOptions = { center: latLng,
+                          zoom: 5, 
+                          mapTypeId: google.maps.MapTypeId.ROADMAP};
+
+        mapObj.googleMap = new google.maps.Map(element, mapOptions);
+    }
+};*/
+
 var map;
 
 //creates blank array for all listings
 var markers = [];
 
 //function to initialize the map
-function initMap() {
+var initMap = function() {
        
 	map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.759951, lng: -122.415139}, 
-    zoom: 4
+    zoom: 15
    });
  
  var largeInfoWindows = new google.maps.InfoWindow();
       var bounds = new google.maps.LatLngBounds();
 
-      for (var i = 0; i < locations.length; i++) {
+      for (var i = 0; i < initialLocations.length; i++) {
         //get position from location array
-        var position = initialLocations.[i].latLng;
-        var address = initialLocations.[i].address;
-        var title = initialLocations.[i].name;
+        var position = initialLocations[i].coordinates;
+        var address = initialLocations[i].address;
+        var title = initialLocations[i].name;
         //create marker per locations and put into markers array
         var marker = new google.maps.Marker({
-          //map: map,
+          map: map,
           position: position,
           title: title,
           address: address,
@@ -89,5 +110,5 @@ function initMap() {
           populateInfoWindow(this, largeInfoWindows);
         });
       }
-
+};
 ko.applyBindings(new ViewModel());
