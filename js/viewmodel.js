df6873d61d3c3308adbe6c036eeba608881b58d1
Wu-Tang-Name-Here <einarsevilla@gmail.com>
2017-11-27 21:49:49 -0800

    Add files via upload

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -163,5 +163,29 @@ var initMap = function() {
             infowindow.addListener('closeclick', function(){
             })
         }
+    };
+
+    /////ajax request for FourSquare API
+    var foursquareRequest = function (marker) {
+        var apiURL = 'https://api.foursquare.com/v2/venues/';
+        var foursquareClientID = 'YKM1SQVAHAI2ERVFQZNT1BXARDYSGBEACCAOKPTAWLMNVNCK'
+        var foursquareSecret ='2U0X5KPXABRQKNKB1VPTEWYN3SVA0NKMFC1VMQEGMPECEZFB';
+        var foursquareVersion = '20170112';
+        var venueFoursquareID = marker.id;
+
+        var foursquareURL = apiURL + venueFoursquareID + '?client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
+
+        $.ajax({
+          url: foursquareURL,
+          success: function(data) {
+            console.log(data);
+
+            var rating = data.response.venue.rating;
+            var hours = data.response.venue.hours;
+            var menu = data.response.venue.menu;
+            var description = data.response.venue.description;
+          }
+        });
     }
+
 };
